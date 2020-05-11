fe9e0a8eb6f9cbb95654b49dc2135ac8a555bac9
ZoeLiao <zoejoyuliao@gmail.com>
2020-05-12 00:44:23 +0800

    Add swagger_auto_schema

--- src/api/user/views.py
+++ src/api/user/views.py
@@ -1,7 +1,12 @@
 from django.http import JsonResponse
 from django.db import transaction
 from rest_framework.generics import GenericAPIView
-
+from rest_framework.parsers import (
+    FormParser,
+    MultiPartParser
+)
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from user.serializers import UserSerializer
 from user.models import User
 
@@ -10,13 +15,48 @@ class UsersView(GenericAPIView):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    parser_classes = (FormParser, MultiPartParser)
 
+    @swagger_auto_schema(
+        operation_summary='我是 GET 的摘要',
+        operation_description='我是 GET 的說明',
+        manual_parameters=[
+           openapi.Parameter(
+               name='email',
+               in_=openapi.IN_PATH,
+               description='Email',
+               type=openapi.TYPE_STRING
+           ),
+
+        ]
+    )
     def get(self, request, *args, **krgs):
         users = self.get_queryset()
         serializer = self.serializer_class(users, many=True)
         data = serializer.data
         return JsonResponse(data, safe=False)
 
+    @swagger_auto_schema(
+        operation_summary='我是 POST 的摘要',
+        operation_description='我是 POST 的說明',
+        # request_body=openapi.Schema(
+        #     type=openapi.TYPE_OBJECT,
+        #     properties={
+        #         'name': openapi.Schema(
+        #             type=openapi.TYPE_STRING,
+        #             description='User Name'
+        #         )
+        #     }
+        # )
+        manual_parameters=[
+           openapi.Parameter(
+               name='image',
+               in_=openapi.IN_FORM,
+               description='Image',
+               type=openapi.TYPE_FILE
+           ),
+       ]
+    )
     def post(self, request, *args, **krgs):
         data = request.data
         try:
