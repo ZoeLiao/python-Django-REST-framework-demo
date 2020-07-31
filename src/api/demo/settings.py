cb9191a67281936f4f0f0968e492a87e364b67cd
ZoeLiao <zoejoyuliao@gmail.com>
2020-07-31 23:34:16 +0800

    Modify README.md and remove unused apps and middlewares

--- src/api/demo/settings.py
+++ src/api/demo/settings.py
@@ -34,8 +34,6 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     # package
     'rest_framework',
@@ -46,11 +44,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
